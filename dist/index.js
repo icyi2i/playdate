c92c93214dedaae091f9acb62ccb9a5ec7721ac9
icyi2i <sarthak3gambhir@gmail.com>
2022-01-09 23:35:21 +0530

    Add library compile options with babel and crossenv. Add demo folder for gh-pages build

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DateInput", {
+  enumerable: true,
+  get: function get() {
+    return _DateInput.default;
+  }
+});
+
+var _DateInput = _interopRequireDefault(require("./DateInput"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
